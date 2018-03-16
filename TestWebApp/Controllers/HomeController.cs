1bc65c1424dfae368caa3bbcf9beb0d1d74c6bcc
anulekha <anulekha.k@gmail.com>
2018-03-16 06:52:06 +0000

    test sonar

--- TestWebApp/Controllers/HomeController.cs
+++ TestWebApp/Controllers/HomeController.cs
@@ -16,6 +16,7 @@ namespace TestWebApp.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
+            throw new NullReferenceException();
 
             return View();
         }
